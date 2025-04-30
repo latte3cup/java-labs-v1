49d520eb1803bfe094498bc9ec6ab24dc491507f
DESKTOP-GR883KV\user <`12123>
2025-04-30 15:53:59 +0900

    Chap 4 trying

--- chapter4/labs/lab1/Person.java
+++ chapter4/labs/lab1/Person.java
@@ -7,14 +7,22 @@ package chapter4.labs.lab1;
  */
 public class Person {
     // TODO: 이름, 나이, 성별을 저장할 수 있는 필드를 정의하세요.
-    
+    private  String name;
+    private  int age;
+    private  boolean gender; // 1: 남 // 0:여
     
     
     // TODO: 객체 정보를 출력하는 메소드를 작성하세요.
-    
+    public void info(){
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Gender: " + gender);
+    }
     
     
     // TODO: 생일이 지났을 때 나이를 한 살 증가시키는 메소드를 작성하세요.
-    
+    public void addAge(Person person){
+        this.age += 1;
+    }
     
 } 
